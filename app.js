ec213ea428e709b09e3bcb9f754965413e5d71e4
keb <keb@sheev.net>
2020-08-08 13:26:24 -0400

    functions

---
+++ app.js
@@ -0,0 +1,14 @@
+(async function() {
+    function get(url, params) {
+        const query = Object.entries(params)
+            .map(a => encodeURIComponent(a[0]) + '=' + encodeURIComponent(a[1]))
+            .join('&')
+        ;
+
+        return fetch(url + '?' + query);
+    }
+
+    let foo = await get('/.netlify/functions/notes');
+    const json = await foo.json();
+    console.log(json);
+})();
---
+++ functions/notes.js
@@ -0,0 +1,10 @@
+exports.handler = function(event, context, callback) {
+    console.log(event);
+    console.log(context);
+    console.log(callback);
+
+    callback(null, {
+        statusCode: 200,
+        body: 'hello world'
+    });
+};
